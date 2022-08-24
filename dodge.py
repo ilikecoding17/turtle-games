4de9e4a88927a7931f3f35ec4dd1e7fe0471434a
isaac <isaaccraciun13@gmail.com>
2022-08-24 19:35:45 +0100

    Refactored dodge game: - initial refactor

--- dodge.py
+++ dodge.py
@@ -1,14 +1,15 @@
 import turtle
 from turtle import *
-import time
-import random
 from random import randint
 
+# leave for later
 wn = turtle.Screen()
 wn.title("dodge! by Isaac and Adam")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+wn.listen()
 
 # score
 score_a = 0
@@ -16,46 +17,54 @@ score_b = 0
 level = 1
 
 
-# paddle a
-paddle_a = turtle.Turtle()
-paddle_a.speed(0)
-paddle_a.shape("square")
-paddle_a.penup()
-paddle_a.goto(0, 0)
+def create_player():
+    player_shape = turtle.Turtle()
+    player_shape.speed(0)
+    player_shape.shape("square")
+    player_shape.penup()
+    player_shape.goto(0, 0)
+
+    return player_shape
 
 
-red = turtle.Turtle()
-red.speed(0)
-red.shape("square")
-red.color("red")
-red.shapesize(stretch_wid=2, stretch_len=2)
-red.penup()
-red.goto(-290, 270)
-red.dx = 0.02
-red.dy = -0.02
+player_one = create_player()
 
 
-red2 = turtle.Turtle()
-red2.speed(0)
-red2.shape("square")
-red2.color("red")
-red2.shapesize(stretch_wid=2, stretch_len=2)
-red2.penup()
-red2.goto(-290, 270)
-red2.dx = 0.1
-red2.dy = -0.2
+def create_enemy():
+    enemy_shape = turtle.Turtle()
+    enemy_shape.speed(0)
+    enemy_shape.shape("square")
+    enemy_shape.color("red")
+    enemy_shape.shapesize(stretch_wid=2, stretch_len=2)
+    enemy_shape.penup()
+    enemy_shape.goto(randint(-290, 290), randint(-390, 390))
+    enemy_shape.dx = 0.02
+    enemy_shape.dy = -0.02
 
-gren = turtle.Turtle()
-gren.speed(0)
-gren.shape("square")
-gren.color("green")
-gren.shapesize(stretch_wid=2, stretch_len=2)
-gren.penup()
-gren.goto(-290, 270)
-gren.dx = 0.2
-gren.dy = -0.1
+    return enemy_shape
 
 
+enemy_one = create_enemy()
+enemy_two = create_enemy()
+
+
+def create_ally():
+    ally_shape = turtle.Turtle()
+    ally_shape.speed(0)
+    ally_shape.shape("square")
+    ally_shape.color("green")
+    ally_shape.shapesize(stretch_wid=2, stretch_len=2)
+    ally_shape.penup()
+    ally_shape.goto(randint(-290, 290), randint(-390, 390))
+    ally_shape.dx = 0.2
+    ally_shape.dy = -0.1
+
+    return ally_shape
+
+
+ally_one = create_ally()
+
+# leave for later
 pen = turtle.Turtle()
 pen.speed(0)
 pen.color("white")
@@ -68,126 +77,127 @@ pen.write(
 
 
 # functions
-def paddle_a_up():
-    y = paddle_a.ycor()
-    y += 20
-    paddle_a.sety(y)
+def move_shape_up(shape):
+    def helper():
+        y = shape.ycor()
+        y += 20
+        shape.sety(y)
+
+    return helper
 
 
-def paddle_a_down():
-    y = paddle_a.ycor()
-    y -= 20
-    paddle_a.sety(y)
-    red.dx += 0.001
+def move_shape_down(shape):
+    def helper():
+        y = shape.ycor()
+        y -= 20
+        shape.sety(y)
+
+    return helper
 
 
-def paddle_a_right():
-    X = paddle_a.xcor()
-    X += 20
-    paddle_a.setx(X)
+def move_shape_right(shape):
+    def helper():
+        x = shape.xcor()
+        x += 20
+        shape.setx(x)
+
+    return helper
 
 
-def paddle_a_left():
-    X = paddle_a.xcor()
-    X -= 20
-    paddle_a.setx(X)
+def move_shape_left(shape):
+    def helper():
+        x = shape.xcor()
+        x -= 20
+        shape.setx(x)
+
+    return helper
 
 
-# keyboard binding
-wn.listen()
-wn.onkeypress(paddle_a_up, "w")
+def make_shape_moveable(shape, key_up, key_down, key_left, key_right):
+    wn.onkeypress(move_shape_up(shape), key_up)
+    wn.onkeypress(move_shape_down(shape), key_down)
+    wn.onkeypress(move_shape_left(shape), key_left)
+    wn.onkeypress(move_shape_right(shape), key_right)
 
-wn.listen()
-wn.onkeypress(paddle_a_down, "s")
 
-wn.listen()
-wn.onkeypress(paddle_a_right, "d")
-
-wn.listen()
-wn.onkeypress(paddle_a_left, "a")
+make_shape_moveable(player_one, "w", "s", "a", "d")
 
 
 while True:
     wn.update()
 
-    red.setx(red.xcor() + red.dx)
-    red.sety(red.ycor() + red.dy)
+    enemy_one.setx(enemy_one.xcor() + enemy_one.dx)
+    enemy_one.sety(enemy_one.ycor() + enemy_one.dy)
 
-    red2.setx(red2.xcor() + red2.dx)
-    red2.sety(red2.ycor() + red2.dy)
+    enemy_two.setx(enemy_two.xcor() + enemy_two.dx)
+    enemy_two.sety(enemy_two.ycor() + enemy_two.dy)
 
-    red.setx(red.xcor() + red.dx)
-    red.sety(red.ycor() + red.dy)
+    ally_one.setx(ally_one.xcor() + enemy_one.dx)
+    ally_one.sety(ally_one.ycor() + enemy_one.dy)
 
-    red2.setx(red2.xcor() + red2.dx)
-    red2.sety(red2.ycor() + red2.dy)
+    if enemy_one.ycor() > 290:
+        enemy_one.sety(290)
+        enemy_one.dy *= -1
 
-    gren.setx(gren.xcor() + red.dx)
-    gren.sety(gren.ycor() + red.dy)
+    if enemy_one.ycor() < -290:
+        enemy_one.sety(-290)
+        enemy_one.dy *= -1
 
-    gren.setx(gren.xcor() + gren.dx)
-    gren.sety(gren.ycor() + gren.dy)
+    if enemy_one.xcor() > 390:
+        enemy_one.setx(390)
+        enemy_one.dx *= -1
 
-    if red.ycor() > 290:
-        red.sety(290)
-        red.dy *= -1
+    if enemy_one.xcor() < -390:
+        enemy_one.setx(-390)
+        enemy_one.dx *= -1
 
-    if red.ycor() < -290:
-        red.sety(-290)
-        red.dy *= -1
+    if enemy_two.ycor() > 290:
+        enemy_two.sety(290)
+        enemy_two.dy *= -1
 
-    if red.xcor() > 390:
-        red.setx(390)
-        red.dx *= -1
+    if enemy_two.ycor() < -290:
+        enemy_two.sety(-290)
+        enemy_two.dy *= -1
 
-    if red.xcor() < -390:
-        red.setx(-390)
-        red.dx *= -1
+    if enemy_two.xcor() > 390:
+        enemy_two.setx(390)
+        enemy_two.dx *= -1
 
-    if red2.ycor() > 290:
-        red2.sety(290)
-        red2.dy *= -1
+    if enemy_two.xcor() < -390:
+        enemy_two.setx(-390)
+        enemy_two.dx *= -1
 
-    if red2.ycor() < -290:
-        red2.sety(-290)
-        red2.dy *= -1
+    if ally_one.ycor() > 290:
+        ally_one.sety(290)
+        ally_one.dy *= -1
 
-    if red2.xcor() > 390:
-        red2.setx(390)
-        red2.dx *= -1
+    if ally_one.ycor() < -290:
+        ally_one.sety(-290)
+        ally_one.dy *= -1
 
-    if red2.xcor() < -390:
-        red2.setx(-390)
-        red2.dx *= -1
+    if ally_one.xcor() > 390:
+        ally_one.setx(390)
+        ally_one.dx *= -1
 
-    if gren.ycor() > 290:
-        gren.sety(290)
-        gren.dy *= -1
+    if ally_one.xcor() < -390:
+        ally_one.setx(-390)
+        ally_one.dx *= -1
 
-    if gren.ycor() < -290:
-        gren.sety(-290)
-        gren.dy *= -1
+    # if player_one.xcor()
 
-    if gren.xcor() > 390:
-        gren.setx(390)
-        gren.dx *= -1
+    if player_one.xcor() == (0):
+        player_one.color("blue")
 
-    if gren.xcor() < -390:
-        gren.setx(-390)
-        gren.dx *= -1
-
-    # if paddle_a.xcor()
-
-    if paddle_a.xcor() == (0):
-        paddle_a.color("blue")
+    else:
+        player_one.color("white")
 
     if (
-        red.xcor() - 25 <= paddle_a.xcor()
-        and paddle_a.xcor() <= red.xcor() + 25
-        and red.ycor() - 25 <= paddle_a.ycor()
-        and paddle_a.ycor() <= red.ycor() + 25
+        enemy_one.xcor() - 25 <= player_one.xcor()
+        and player_one.xcor() <= enemy_one.xcor() + 25
+        and enemy_one.ycor() - 25 <= player_one.ycor()
+        and player_one.ycor() <= enemy_one.ycor() + 25
     ):
-        paddle_a.goto(randint(-390, 0), randint(0, 390))
+        player_one.goto(randint(-390, 0), randint(0, 390))
         score_a += 1
         score_b -= 1
         pen.clear()
@@ -198,12 +208,12 @@ while True:
         )
 
     if (
-        red2.xcor() - 25 <= paddle_a.xcor()
-        and paddle_a.xcor() <= red2.xcor() + 25
-        and red2.ycor() - 25 <= paddle_a.ycor()
-        and paddle_a.ycor() <= red2.ycor() + 25
+        enemy_two.xcor() - 25 <= player_one.xcor()
+        and player_one.xcor() <= enemy_two.xcor() + 25
+        and enemy_two.ycor() - 25 <= player_one.ycor()
+        and player_one.ycor() <= enemy_two.ycor() + 25
     ):
-        paddle_a.goto(randint(-390, 0), randint(0, 390))
+        player_one.goto(randint(-390, 0), randint(0, 390))
         score_a += 1
         score_b -= 1
         pen.clear()
@@ -213,25 +223,25 @@ while True:
             font=("Courier", 24, "normal"),
         )
 
-    if paddle_a.xcor() > 390:
-        paddle_a.goto(380, paddle_a.ycor())
+    if player_one.xcor() > 390:
+        player_one.goto(380, player_one.ycor())
 
-    if paddle_a.xcor() < -390:
-        paddle_a.goto(-390, paddle_a.ycor())
+    if player_one.xcor() < -390:
+        player_one.goto(-390, player_one.ycor())
 
-    if paddle_a.ycor() > 290:
-        paddle_a.goto(paddle_a.xcor(), 290)
+    if player_one.ycor() > 290:
+        player_one.goto(player_one.xcor(), 290)
 
-    if paddle_a.ycor() < -290:
-        paddle_a.goto(paddle_a.xcor(), -280)
+    if player_one.ycor() < -290:
+        player_one.goto(player_one.xcor(), -280)
 
     if (
-        gren.xcor() - 25 <= paddle_a.xcor()
-        and paddle_a.xcor() <= gren.xcor() + 25
-        and gren.ycor() - 25 <= paddle_a.ycor()
-        and paddle_a.ycor() <= gren.ycor() + 25
+        ally_one.xcor() - 25 <= player_one.xcor()
+        and player_one.xcor() <= ally_one.xcor() + 25
+        and ally_one.ycor() - 25 <= player_one.ycor()
+        and player_one.ycor() <= ally_one.ycor() + 25
     ):
-        paddle_a.goto(randint(-390, 0), randint(0, 390))
+        player_one.goto(randint(-390, 0), randint(0, 390))
         score_b += 1
         pen.clear()
         pen.write(
@@ -242,10 +252,10 @@ while True:
 
     if score_b == 1:
         level += 1
-        red.dx += 0.2
-        red.dy += 0.2
-        red2.dx += 0.2
-        red2.dx += 0.2
+        enemy_one.dx += 0.2
+        enemy_one.dy += 0.2
+        enemy_two.dx += 0.2
+        enemy_two.dx += 0.2
         score_b += 2
         pen.clear()
         pen.write(
